b2069416749d23d5d0e16de06278bc4d76f062dc
eirikkar <eirikkar@proton.me>
2024-11-14 10:54:31 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+
+namespace ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AnsiConsole.Markup("[underline red]Hello[/] World!");
+        // Ask for the user's favorite fruit
+        var fruit = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+                .Title("What's your [green]favorite fruit[/]?")
+                .PageSize(10)
+                .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+                .AddChoices(
+                    new[]
+                    {
+                        "Apple",
+                        "Apricot",
+                        "Avocado",
+                        "Banana",
+                        "Blackcurrant",
+                        "Blueberry",
+                        "Cherry",
+                        "Cloudberry",
+                        "Cocunut",
+                    }
+                )
+        );
+
+        // Echo the fruit back to the terminal
+        AnsiConsole.WriteLine($"I agree. {fruit} is tasty!");
+    }
+}
