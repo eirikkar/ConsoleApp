f62af88933ef66e0d44d0dc9625c30fab1bcb465
eirikkar <eirikkar@proton.me>
2024-11-14 10:55:50 +0100

    confirmation example

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        // Install with 'dotnet add package Spectre.Console' and
+        // 'dotnet add package Spectre.Console.Cli'
         AnsiConsole.Markup("[underline red]Hello[/] World!");
         // Ask for the user's favorite fruit
         var fruit = AnsiConsole.Prompt(
@@ -32,5 +34,17 @@ class Program
 
         // Echo the fruit back to the terminal
         AnsiConsole.WriteLine($"I agree. {fruit} is tasty!");
+
+        // Ask the user to confirm
+        var confirmation = AnsiConsole.Prompt(
+            new TextPrompt<bool>("Run prompt example?")
+                .AddChoice(true)
+                .AddChoice(false)
+                .DefaultValue(true)
+                .WithConverter(choice => choice ? "y" : "n")
+        );
+
+        // Echo the confirmation back to the terminal
+        Console.WriteLine(confirmation ? "Confirmed" : "Declined");
     }
 }
